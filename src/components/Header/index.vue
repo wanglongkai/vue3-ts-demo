d61d076d3988ed3360aa5e97231e567f134ef7ca
wanglongkai <13699283312@163.com>
2023-05-31 20:04:56 +0800

    feat: 自定义指令练习

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,9 +1,24 @@
 <script setup lang='ts'>
-
+import { ObjectDirective } from 'vue';
+// 自定义局部指令，注意命令规则，不然模板中访问不到。自定义全局指令用app.derective()
+const vDebounceClick: ObjectDirective = {
+  mounted: (el: HTMLElement, binding) => {
+    let timer: number;
+    el.addEventListener('click', () => {
+      if (timer) {
+        clearTimeout(timer)
+      }
+      timer = setTimeout(binding.value, Number(binding.arg))
+    })
+  }
+}
+function onClick() {
+  console.log("Only triggered once when clicked many times quickly")
+}
 </script>
 
 <template>
-  <p class="color">
+  <p v-debounce-click:200="onClick" class="color">
     this is header page;
   </p>
 </template>
