7225e2d21718df3f8de7b627071c335a9fab3caf
wanglongkai <13699283312@163.com>
2023-06-08 10:08:28 +0800

    feat: 添加pinia getter使用例子

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,19 +1,29 @@
 import axios from 'axios';
 import { defineStore } from 'pinia';
-import { reactive, toRefs } from 'vue';
+import { reactive, toRefs, computed } from 'vue';
 
 const useUserStore = defineStore('user', () => {
+  // state
   const user = reactive({
     name: 'wlk',
     age: 25,
     count: 100
   })
+
+  // getter
+  const doubleCount = computed(() => {
+    return user.count * 2;
+  })
+
+  // action, 可以有异步操作
   const getUser = async () => {
     const res = await axios.get("https://jsonplaceholder.typicode.com/users");
     user.name = res.data[0].name;
   }
+
   return {
     ...toRefs(user),
+    doubleCount,
     getUser
   }
 })
--- src/views/store/index.vue
+++ src/views/store/index.vue
@@ -1,5 +1,6 @@
 <template>
-  {{ name }} 
+  {{ name }}--{{ doubleCount }}--{{ count }}
+  <el-button @click="changeCount">count++</el-button>
   <el-button @click="changeName">changeName</el-button>
   <el-button @click="changeRemoteName">changeRemoteName</el-button>
 </template>
@@ -7,13 +8,16 @@
 <script lang='ts' setup>
   import useUserStore from '@/stores';
   import { storeToRefs } from 'pinia';
-  const { name } = storeToRefs(useUserStore());
+  const { name, doubleCount, count } = storeToRefs(useUserStore());
   const { getUser }  = useUserStore(); // action需要直接解构，而state需要storeToRefs解构
   const changeName = () => {
     name.value = 'wanglongkai'
   }
   const changeRemoteName = () => {
     getUser();
+  }
+  const changeCount = () => {
+    count.value++
   }
 </script>
 
